70e41f01b7dbfa31d3442128b8fba1aa5ff895cb
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-02-22 19:31:00 +0000

    Update euler_step.py

--- euler_step.py
+++ euler_step.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def euler_step(stepsize,x_n,t_n):
     return x_n + stepsize*x_n, t_n + stepsize
 
@@ -11,10 +10,11 @@ def expression(x,t):
 
 def solve_to(x_1,t_1,t_2,stepsize):
     while t_1 < t_2:
-        x_1,t_1 = euler_step(stepsize, x_1, t_1)
+        delta_t = t_2 - t_1
+        if delta_t < stepsize:
+            stepsize = delta_t
+        x_1,t_1 = euler_step(stepsize, x_1, t_1)  
     return x_1, t_1
-
-
 
 
 stepsize = 0.125
