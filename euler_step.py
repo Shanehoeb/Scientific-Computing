5e00c1b62e95707b6319db8c59e70e2e15cdca6d
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-03-07 14:00:07 +0000

    Broaden code scope by setting function as a parameters rather than hardcoding for the assignement

--- euler_step.py
+++ euler_step.py
@@ -1,6 +1,5 @@
-def euler_step(stepsize,x_n,t_n):
+def euler_step(stepsize,func,x_n,t_n):
     #Performs a single euler step for a value of x, t and a stepsize
-    # Input : x_n,t_n pair of coordinates, stepsize
+    # Input : x_n,t_n pair of coordinates, stepsize,func the ODE
     # Output : Updated pair of values after euler step of stepsize
-    return x_n + stepsize*x_n, t_n + stepsize
-    
+    return x_n + stepsize*func(x_n,t_n), t_n + stepsize
