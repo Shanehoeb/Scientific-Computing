a2bdff950e1f5b71aee8f9dc05baadc8f8327f6d
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-03-07 19:12:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ import matplotlib.pyplot as plt
 from solve_ode import solve_ode
 
 #Params
-stepsize_list = []
-step = 0
-for i in range(0,10000):
-    step += 1/10000
-    stepsize_list.append(step)
+stepsize_list = np.logspace(-8,-1,21,dtype='float64')
     
 x_0 = 1
 t_0 = 0
-t_array = np.random.rand(10)
-t_array = np.append(t_array,1)
+t_array = np.linspace(0,1,21,dtype='float64')
+
 deltat_max = 2
 
 error_list = []
+method = 'euler'
 
 def func(x,t):
     return x
@@ -23,9 +20,10 @@ def func(x,t):
 if __name__ == '__main__':
 
     for stepsize in stepsize_list:
-        sol = solve_ode(x_0,t_0,t_array,stepsize,deltat_max,func)
+        sol = solve_ode(x_0,t_0,t_array,stepsize,deltat_max,func,method)
         error = np.absolute(np.exp(1)-sol[1][-1])
         error_list.append(error)
+        
 
     fig = plt.figure()
     ax = plt.gca()
