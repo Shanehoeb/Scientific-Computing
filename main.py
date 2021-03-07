92dbe07f0a1e9c01d80d42721c5bb8189af83ef1
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-03-07 14:00:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  
 import numpy as np
 import matplotlib.pyplot as plt
 from solve_ode import solve_ode
@@ -18,10 +17,13 @@ deltat_max = 2
 
 error_list = []
 
+def func(x,t):
+    return x
+
 if __name__ == '__main__':
 
     for stepsize in stepsize_list:
-        sol = solve_ode(x_0,t_0,t_array,stepsize,deltat_max)
+        sol = solve_ode(x_0,t_0,t_array,stepsize,deltat_max,func)
         error = np.absolute(np.exp(1)-sol[1][-1])
         error_list.append(error)
 
@@ -31,4 +33,3 @@ if __name__ == '__main__':
     ax.set_yscale('log')
     ax.set_xscale('log')
     plt.show()
-    
