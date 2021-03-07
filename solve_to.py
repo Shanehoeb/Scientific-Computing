98b6cc639fce18588948a15e5fd8b2e3cb8943a1
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-03-07 19:14:13 +0000

    Update solve_to.py

--- solve_to.py
+++ solve_to.py
@@ -1,17 +1,22 @@
 from euler_step import euler_step
+from rk4_step import rk4_step
 
-def solve_to(x_1,t_1,t_2,stepsize,deltat_max,func):
+def solve_to(x_1,t_1,t_2,stepsize,deltat_max,func,method):
     #solves from x_1,t_1 to x_2,t_2 in steps no bigger than deltat_max
     # Input : x_1,t_1 pair of coordinates, t_2 time at which we want to approximate,
     #         stepsize of the euler step and the maximum difference between 
     #         input and output time deltat_max
-    # Output : updated coordinate pair
     if t_2 - t_1 < deltat_max:
         while t_1 < t_2:
             delta_t = t_2 - t_1
             if delta_t < stepsize:
                 stepsize = delta_t
-            x_1,t_1 = euler_step(stepsize,func, x_1, t_1)  
+            if method == 'euler':
+                x_1 = euler_step(stepsize,func, x_1, t_1)[0]
+            if method == 'rk4':
+                x_1 = rk4_step(stepsize,func, x_1, t_1)[0]
+
+            t_1 += stepsize
         return x_1, t_1
     else:
         print("Difference is too big ! Delta t max exceeded.")
