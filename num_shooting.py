583837da0968cb0c2ebd7de01af6f955bc090ead
Shanehoeb <db18316@bristol.ac.uk>
2021-05-16 12:06:39 +0100

    Working for pred-prey and scipy functions

--- num_shooting.py
+++ num_shooting.py
@@ -4,24 +4,21 @@ import matplotlib.pyplot as plt
 from solve_ode import solve_ode
 from math import nan
 
-def pred_prey(t,z):
+def pred_prey(t,z,b):
     x, y = z
     a = 1.
     d = 0.1
-    b = 0.2
-
     dxdt = x*(1-x) - (a*x*y)/(d+x)
     dydt = b*y*(1 - (y/x))
-    print(np.array((dxdt, dydt)))
     return np.array((dxdt, dydt))
 
 
-sol = solve_ivp(pred_prey, [0, 100], [1,1],dense_output=True)
-t = np.linspace(0, 100, 501)
-z = sol.sol(t)
-import matplotlib.pyplot as plt
-plt.plot(t, z.T)
-plt.xlabel('t')
-plt.legend(['x', 'y'], shadow=True)
-plt.title('Lotka-Volterra System')
+sol = solve_ivp(lambda t,z:pred_prey(t,z,0.4), [0, 100], [4,1],dense_output=True)
+plt.plot(sol.y[0, :], sol.y[1, :])
+sol = solve_ivp(lambda t,z:pred_prey(t,z,0.4), [0, 100], [10,1],dense_output=True)
+plt.plot(sol.y[0, :], sol.y[1, :])
+sol = solve_ivp(lambda t,z:pred_prey(t,z,0.4), [0, 100], [5,5],dense_output=True)
+plt.plot(sol.y[0, :], sol.y[1, :])
+sol = solve_ivp(lambda t,z:pred_prey(t,z,0.4), [0, 100], [0.1,0],dense_output=True)
+plt.plot(sol.y[0, :], sol.y[1, :])
 plt.show()
